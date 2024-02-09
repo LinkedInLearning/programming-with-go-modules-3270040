69163258f516036f08c4f58c6e506d71939e17cf
Vladimir Vivien <vladimir.vivien@gmail.com>
2024-02-08 20:48:40 -0500

    Using pre-released versions

--- emoserv/main.go
+++ emoserv/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/http"
 
-	"lil/emojiis/search"
+	"github.com/vladimirvivien/emojiis/search"
 )
 
 func main() {
@@ -20,6 +20,23 @@ func main() {
 		// write result
 		w.Header().Set("Content-Type", "application/json")
 		result := search.ByDescription(params)
+		if err := json.NewEncoder(w).Encode(&result); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+
+	http.HandleFunc("/search/tags", func(w http.ResponseWriter, r *http.Request) {
+		var tags []string
+		if err := json.NewDecoder(r.Body).Decode(&tags); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		log.Printf("Got request: %#v", tags)
+
+		// write result
+		w.Header().Set("Content-Type", "application/json")
+		result := search.ByTags(tags...)
 		if err := json.NewEncoder(w).Encode(&result); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
